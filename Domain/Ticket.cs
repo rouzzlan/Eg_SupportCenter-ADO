40eb8c82ea35f22cb45604489338c637afdda551
rouslankhayaouri <rouslan_kh@hotmail.com>
2015-12-03 01:24:09 +0100

    oplossing Supportcenter opgave

---
+++ Domain/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC.BL.Domain
+{
+  public class Ticket
+  {
+    public int TicketNumber { get; set; }
+    public int AccountId { get; set; }
+    [Required]
+    [MaxLength(100, ErrorMessage="Er zijn maximaal 100 tekens toegestaan")]
+    public string Text { get; set; }
+    public DateTime DateOpened { get; set; }
+    public TicketState State { get; set; }
+
+    public ICollection<TicketResponse> Responses { get; set; }
+  }
+}
